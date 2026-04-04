3f272562e6a56ab9c8d3545d2298960e7e67f055
hyeonseo9999 <gustj5161@gmail.com>
2026-04-05 04:10:38 +0900

    edit README

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    context = {
+        'generation': 14,                   
+        'info': {                           
+            'weather': 'View 작성 및 URL 연결',
+            'feeling': 'Mainpage 작성과 동일',
+            'note': 'Template 상속을 통한 중복되는 부분 분리, navbar 분리 학습 및 CSS, image 적용 학습'
+        }
+    }
+    return render(request, 'main/mainpage.html', context)
+
+def secondpage(request):
+    return render(request, 'main/secondpage.html')
