9a33c756cf8d9bb7c2645516c406c75e8a59304a
hyeonseo9999 <gustj5161@gmail.com>
2026-04-05 12:59:21 +0900

    edit README

--- main/views.py
+++ main/views.py
@@ -6,9 +6,9 @@ def mainpage(request):
     context = {
         'generation': 14,                   
         'info': {                           
-            'weather': 'View 작성 및 URL 연결',
-            'feeling': 'Mainpage 작성과 동일',
-            'note': 'Template 상속을 통한 중복되는 부분 분리, navbar 분리 학습 및 CSS, image 적용 학습'
+            'mainpage': 'View 작성 및 URL 연결',
+            'secondpage': 'Mainpage 작성과 동일',
+            'etc': 'Template 상속을 통한 중복되는 부분 분리, navbar 분리 학습 및 CSS, image 적용 학습'
         }
     }
     return render(request, 'main/mainpage.html', context)
