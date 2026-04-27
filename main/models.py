c2f247df771fa5ffcd8b8f703a350b2c5a1d23a8
hyeonseo9999 <gustj5161@gmail.com>
2026-04-27 18:33:26 +0900

    0408 과제 제출

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+     title = models.CharField(max_length=50)
+     writer = models.CharField(max_length=30)
+     content = models.TextField()
+     pub_date = models.DateTimeField()
+     category = models.CharField(max_length=10)
+
+     def summary(self):
+          return self.content[:20]
